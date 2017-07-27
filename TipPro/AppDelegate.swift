f34ceb24876e2549fe1a3c9383d29b2f1ddfc32f
Charlothe <cicihe1122@gmail.com>
2017-07-27 13:54:41 +0800

    Add Completed Tip Calculator project

--- TipPro/AppDelegate.swift
+++ TipPro/AppDelegate.swift
@@ -44,3 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    // Override point for customization after application launch.
+    print("Hello Make School!")
+    return true
+}
+
+
+
